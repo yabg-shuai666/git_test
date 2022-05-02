bee644b2c4e0ce90fe8ec48a352c0c5edc1735fd
cmj <1160885841@qq.com>
2022-05-02 21:54:59 +0800

    1-1

--- src/test.java
+++ src/test.java
@@ -6,6 +6,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("test");
-        System.out.println("test1-1");
+        System.out.println("test1.1");
     }
 }
