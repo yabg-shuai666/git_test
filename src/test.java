b149f20eecfda661fc6824a9a236c75e2a2bcbff
yangshuai.sy <yangshuai.sy@alibaba-inc.com>
2022-05-02 20:36:43 +0800

    first commit

---
+++ src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author yang.shuai
+ * @Date 2022/5/2 8:31 PM
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
